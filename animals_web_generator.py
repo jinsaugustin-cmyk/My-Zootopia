1a22d4d67f1b03d664068d89351ad691f07e882c
Jins Augustin <jinsaugustin@gmail.com>
2026-09-12 20:41:35 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,20 +13,23 @@ output = ""
 
 
 for animal in animals_data:
+    # Start one animal card
+    output += '<li class="cards__item">\n'
+
     if "name" in animal:
-        output += f"Name: {animal['name']}\n"
+        output += f"Name: {animal['name']}<br/>\n"
 
     if "diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}\n"
+        output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
 
     if "locations" in animal and animal["locations"]:
-        output += f"Location: {animal['locations'][0]}\n"
+        output += f"Location: {animal['locations'][0]}<br/>\n"
 
     if "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}\n"
+        output += f"Type: {animal['characteristics']['type']}<br/>\n"
 
-    #print()
-    output += "\n"
+    # End one animal card
+    output += "</li>\n"
 
 print(output)
 
